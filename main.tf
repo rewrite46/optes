34e2250a5b16fe10911eac04a0655d131396689c
root <root@localhost.localdomain>
2020-12-28 03:51:11 +0900

    11

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ provider "prismacloud" {
   url = var.url
   protocol = var.protocol
   skip_ssl_cert_verification = var.skip_ssl_cert_verification
-  username = PRISMACLOUD_USERNAME
-  password = PRISMACLOUD_PASSWORD
   
 }
 
